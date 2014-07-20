032237b2160cd630d0002b9739df58a55b400b49
Antonin Décimo <antonin.decimo@gmail.com>
2014-07-21 00:17:21 +0200

    Fix bug where lua and conf.lua were found, but t.version was not specified

--- love-release.sh
+++ love-release.sh
@@ -280,7 +280,7 @@ RELEASE_WIN_32=false
 RELEASE_WIN_64=false
 
 if [ "$FOUND_LUA" = true ] && [ -f "conf.lua" ]; then
-    LOVE_VERSION_AUTO=$(lua -e 'f = loadfile("conf.lua"); t, love = {window = {}, modules = {}}, {}; f(); love.conf(t); print(t.version)')
+    LOVE_VERSION_AUTO=$(lua -e 'f = loadfile("conf.lua"); t, love = {window = {}, modules = {}}, {}; f(); love.conf(t); t.version = t.version or ""; print(t.version)')
 else
     LOVE_VERSION_AUTO=$(grep -Eo -m 1 "t.version = \"[0-9]+.[0-9]+.[0-9]+\"" conf.lua 2> /dev/null |  grep -Eo "[0-9]+.[0-9]+.[0-9]+")
 fi
